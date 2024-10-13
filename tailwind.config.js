a180fd1bd116f47e1069e736b5e4e0cb051a4fb4
Danial Alborz <danialalborz3@gmail.com>
2024-10-13 04:18:37 -0700

    add matrix bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "move-icon-row-odd": {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+        "move-icon-row-even": {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        'move-icon-row-odd': 'move-icon-row-odd 80s linear infinite alternate',
+        'move-icon-row-even': 'move-icon-row-even 80s linear infinite alternate',
+      }
+    },
   },
   plugins: [],
 }
